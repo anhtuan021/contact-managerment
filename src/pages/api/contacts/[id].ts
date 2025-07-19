386139b1e3d3bb9fabc00e8f09b8c3845913bbf5
LAPTOP-Q462S81B\ASUS <doantrungminh20@gmail.com>
2025-07-19 12:23:37 +0700

    contact

--- src/pages/api/contacts/[id].ts
+++ src/pages/api/contacts/[id].ts
@@ -16,6 +16,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       try {
         const updated = await Contact.findByIdAndUpdate(id, req.body, { new: true });
         return res.status(200).json(updated);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } catch (err: any) {
         return res.status(400).json({ error: err.message });
       }
